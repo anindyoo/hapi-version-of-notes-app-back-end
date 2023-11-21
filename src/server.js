a051478f24992e85aa8da93f80440d26e604cab3
anindyoo <anindyo0216@gmail.com>
2023-11-21 19:53:42 +0700

    feat: add changes with postgres

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -28,24 +30,3 @@ const init = async () => {
 };
  
 init();
-// const Hapi = require('@hapi/hapi');
-// const routes = require('./routes');
-
-// const init = async () => {
-//   const server = Hapi.server({
-//     port: 5000,
-//     host: 'localhost',
-//     routes: {
-//       cors: {
-//         origin: ['*'],
-//       },
-//     },
-//   });
-
-//   server.route(routes);
-
-//   await server.start();
-//   console.log(`Server berjalan pada ${server.info.uri}`);
-// };
-
-// init();
